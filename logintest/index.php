bc3ebe0869411aa667243930ff9952b71f5e95a7
Átila <157402682+ATILAGATTO@users.noreply.github.com>
2024-05-22 13:17:47 -0300

    Essa é a pasta do painel de login incompleto

---
+++ logintest/index.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Entrar</title>
+  <link href="teste.css" rel="stylesheet">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+  <header class="header">
+    <nav class="navbar">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Contact</a>
+    </nav>
+
+    <form action="#" class="search-bar">
+      <input type="text" placeholder="Buscar...">
+      <button type="submit"><i class='bx bx-search'></i></button>
+    </form>
+  </header>
+
+  <div class="background"></div>
+  <div class="container">
+    <div class="content">
+      <h2 class="logo"><i class='bx bxl-firebase'></i>Codehal</h2>
+
+      <div class="text-sci">
+        <h2>Welcome!<br><span>To Our New Website.</span></h2>
+
+        <p>Loren hggdfjhvhmfjglhflhgkhkmhm</p>
+
+        <div class="social-icons">
+          <a href="#"><i class='bx bxl-linkedin'></i></a>
+          <a href="#"><i class='bx bxl-facebook-circle'></i></a>
+          <a href="#"><i class='bx bxl-instagram'></i></a>
+          <a href="#"><i class='bx bxl-twitter'></i></a>
+        </div>
+      </div>
+    </div>
+
+    <div class="logreg-box">
+      <div class="form-box login">
+        <form action="#">
+          <h2>Sign In</h2>
+
+          <div class="input-box">
+            <sapn class="icon"><i class='bx bxs-envelope'></i></sapn>
+            <input type="email" required>
+            <label>Email</label>
+          </div>
+
+          <div class="input-box">
+            <sapn class="icon"><i class='bx bxs-lock-alt'></i></sapn>
+            <input type="password" required>
+            <label>Password</label>
+          </div>
+
+          <div class="remember-forgot">
+            <label><input type="checkbox"> Remember me</label>
+            <a href="#">Forgot password?</a>
+          </div>
+          <button type="submit" class="btn">Sign In</button>
+
+          <div class="login-register">
+            <p>Don't have an account? <a href="#" class="register-link">Sign up</a></p>
+          </div>
+        </form>
+      </div>
+
+      <div class="form-box register">
+        <form action="#">
+          <h2>Sign I</h2>
+
+          <div class="input-box">
+            <sapn class="icon"><i class='bx bxs-envelope'></i></sapn>
+            <input type="email" required>
+            <label>Email</label>
+          </div>
+
+          <div class="input-box">
+            <sapn class="icon"><i class='bx bxs-lock-alt'></i></sapn>
+            <input type="password" required>
+            <label>Password</label>
+          </div>
+
+          <div class="remember-forgot">
+            <label><input type="checkbox"> Remember me</label>
+            <a href="#">Forgot password?</a>
+          </div>
+          <button type="submit" class="btn">Sign In</button>
+
+          <div class="login-register">
+            <p>Don't have an account? <a href="#" class="login-link">Sign up</a></p>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+
+
+  <script src="script.js"></script>
+</body>
+
+</html>
